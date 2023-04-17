ca5fd4badfd6569819c82a5f43d50a7196b20da6
darshita2304 <dppaghadal@gmail.com>
2023-04-17 19:44:24 +0530

    moduler files implementation

---
+++ README.md
@@ -0,0 +1,15 @@
+## Restaurant Product Delivery Time Prediction
+
+### created a environment
+
+```
+conda create -p restoenv python==3.8
+
+conda activate restoenv/
+```
+
+### Install all necessary libraries
+
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+# this will create overall project
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='RestoDelPredoct',
+    version='0.0.1',
+    author='Darshita',
+    author_email='dppaghadal@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
